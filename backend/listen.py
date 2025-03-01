fbd2de45560c734e8da32852ffd4ba7b2a50a66b
nwumefr <chibuikenwume@gmail.com>
2025-03-01 16:56:28 -0500

    fixed env problem

--- backend/listen.py
+++ backend/listen.py
@@ -12,15 +12,13 @@ ngport = os.environ['NGROK_PORT']
 
 app = Flask(__name__)
 CORS(app)
-
+# last_request = {"method": None, "data": None}
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
-        if request.method == 'GET':
-            return "This is a GET request"
-        elif request.method == 'POST':
+        if request.method == 'POST':
             data = request.get_json()
-            return jsonify({"message": "This is a POST request", "data": data})
+        return jsonify(data)
 
 if __name__ == '__main__':
         app.run(debug=True, port=ngport)
