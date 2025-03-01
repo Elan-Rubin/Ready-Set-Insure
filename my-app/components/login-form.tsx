e78cbb80dc36ecda7020a9f87718788e32b88da8
elan_rubin <elandanielrubin@gmail.com>
2025-03-01 11:49:44 -0500

    login pushed

--- my-app/components/login-form.tsx
+++ my-app/components/login-form.tsx
@@ -44,12 +44,12 @@ export function LoginForm({
               <Button type="submit" className="w-full">
                 Login
               </Button>
-              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+              {/* <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">
                   Or continue with
                 </span>
-              </div>
-              <div className="grid grid-cols-3 gap-4">
+              </div> */}
+              {/* <div className="grid grid-cols-3 gap-4">
                 <Button variant="outline" className="w-full">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
@@ -77,7 +77,7 @@ export function LoginForm({
                   </svg>
                   <span className="sr-only">Login with Meta</span>
                 </Button>
-              </div>
+              </div> */}
               <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
                 <a href="#" className="underline underline-offset-4">
--- my-app/components/ui/chart.tsx
+++ my-app/components/ui/chart.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import type { LucideIcon } from "lucide-react"
 import { type TooltipProps, Tooltip as RechartsTooltip, ResponsiveContainer } from "recharts"
 
-import { cn } from "@/components/lib/utils"
+import { cn } from "@/lib/utils"
 
 type ChartConfig = {
   [key: string]: {
